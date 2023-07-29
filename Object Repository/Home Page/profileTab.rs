d235bd7134965975c29e243694e8da87fc775245
Rinon Mac <rinon.mac@outlook.com>
2023-07-29 22:08:36 +0700

    Added Homepage Object Repository

---
+++ Object Repository/Home Page/profileTab.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>profileTab</name>
+   <tag></tag>
+   <elementGuidId>8ffd39f4-267f-45db-991f-5bc067c9fa7e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='wow_content wow_side_loggd_usr']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <type>Main</type>
+      <value>//div[@class='wow_content wow_side_loggd_usr']</value>
+      <webElementGuid>0b31ed4b-3732-4f4c-8d8a-31ac8e2b3f2d</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
