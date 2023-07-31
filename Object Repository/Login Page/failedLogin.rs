9f66d48564ee9c703ae2eb359aa57659df424a39
Rinon Mac <rinon.mac@outlook.com>
2023-07-31 10:27:32 +0700

    Added failed login message object

---
+++ Object Repository/Login Page/failedLogin.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>failedLogin</name>
+   <tag></tag>
+   <elementGuidId>fe16de5e-c66c-459b-82b7-a1443152791a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='alert alert-danger errors']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <type>Main</type>
+      <value>//div[@class='alert alert-danger errors']</value>
+      <webElementGuid>bffad756-016f-441e-a808-a23cfbb11801</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
